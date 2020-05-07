0db10b20c211eb978a8e0b281ab4735c84e05042
Jetty <hanbingdn@163.com>
2020-05-07 09:07:30 +0800

    单摆的简单示例

---
+++ Assets/PendulumScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PendulumScript : MonoBehaviour
+{
+    // 单摆的摆幅
+    public uint maxdegree = 30;
+    public uint step = 5;
+
+    private int degree;
+    private int eachstep;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eachstep = (int)(step * 1);
+    }
+
+    public void Stop()
+    {
+        CancelInvoke("UpdateVarValue");
+    }
+
+    public void Run()
+    {
+        InvokeRepeating("UpdateVarValue", 0, 0.2f);
+    }
+
+    public void NextStep()
+    {
+        if (Mathf.Abs(degree) == maxdegree)
+        {
+            Write.Log("到达最高点");
+            eachstep *= -1;
+        }
+
+        degree += eachstep;
+
+        transform.RotateAround(new Vector3(0, 5, 0), new Vector3(1, 0, 0), eachstep);
+    }
+
+    private void UpdateVarValue()
+    {
+        NextStep();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/UiScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiScript : MonoBehaviour
+{
+    // 单摆
+    public GameObject pendulum;
+
+    private Button btnRunStrop;
+    private Button btnNextStep;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Write.console.LogStart();
+
+        GameObject btnObj = transform.Find("RunStop").gameObject;
+        btnRunStrop = btnObj.GetComponent<Button>();
+        if (btnRunStrop)
+        {
+            btnRunStrop.onClick.AddListener(RunStop);
+        }
+
+        btnObj = transform.Find("Next").gameObject;
+        btnNextStep = btnObj.GetComponent<Button>();
+        if (btnNextStep)
+        {
+            btnNextStep.onClick.AddListener(Next);
+        }
+    }
+
+    void RunStop()
+    {
+        PendulumScript pendulumScript = pendulum.GetComponent<PendulumScript>();
+
+        Text text = btnRunStrop.transform.Find("Text").GetComponent<Text>();
+        if (text.text == "运行")
+        {
+            text.text = "停止";
+            pendulumScript.Run();
+        }
+        else
+        {
+            text.text = "运行";
+            pendulumScript.Stop();
+        }
+    }
+
+    void Next()
+    {
+        PendulumScript pendulumScript = pendulum.GetComponent<PendulumScript>();
+
+        Text text = btnRunStrop.transform.Find("Text").GetComponent<Text>();
+        if (text.text == "运行")
+        {
+            pendulumScript.NextStep();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
